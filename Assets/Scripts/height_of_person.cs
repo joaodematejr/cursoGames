3f092695857ee55aa6545542f1c8c9540621881e
João Dematé Jr <joaodematejr@gmail.com>
2021-02-21 21:32:08 -0300

    Finalização do script

---
+++ Assets/Scripts/height_of_person.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class height_of_person : MonoBehaviour
+{
+    public Transform person;
+    Text text_interface;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text_interface = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("Olá Mundo!!!");
+        //Debug.Log(person.position.y);
+        text_interface.text = Mathf.RoundToInt(person.position.y).ToString();
+    }
+}
